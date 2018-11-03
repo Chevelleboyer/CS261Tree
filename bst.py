2445e3156e1c8df3032c699e21802d1324fe8443
Chevelle Boyer <chevelleboyer@gmail.com>
2018-11-02 22:10:23 -0700

    Made test_insert_right_with_no_child pass.

--- bst.py
+++ bst.py
@@ -1,5 +1,6 @@
-class BinarySearchTree():
-	def __init__(self, value = None, left = None, right = None):
-		self.value = value
-		self.left = left
-		self.right = right
+class BinarySearchTree:
+	def __init__(self, root = None):
+		self.root = root
+		self.leftChild = None
+		self.rightChild = None
+#just do find (acess and find are very similar) find returns the total node with that value, NOT the value the node holds
--- test_bst.py
+++ test_bst.py
@@ -10,9 +10,20 @@ class TestBinarySearchTree(unittest.TestCase):
 	def test_instantiation_with_value(self):
 		fake_value = "Fake"
 		bst = BinarySearchTree(fake_value)
-		self.assertEqual(fake_value, bst.value)
+		self.assertEqual(fake_value, bst.root)
+	#def test_check_left(self):
+
+	#def test_check_right(self):
+
+	def test_insert_right_with_no_child(self):
+		aValue = 55
+		bst = BinarySearchTree()
+		if bst.rightChild == None:
+			bst.rightChild = BinarySearchTree(aValue)
+
 
 	def test_instantiation(self):
+
 		"""
 		A BinarySearchTree exsists.
 		"""
